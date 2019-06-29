d35b15ed19ead5c2c23aa20761f810aa5d581737
drakkai <drakkai@linux.pl>
2019-06-29 16:26:19 +0200

    for checkout

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def 
 
 @app.route('/')
 @app.route('/jobs')
